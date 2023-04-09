e014fa5365769af454f9268478a1b71870c05c8a
laumardi <100428953@alumnos.uc3m.es>
2023-04-09 17:46:51 +0200

    player recibe mensajes del controller

--- www/player/script.js
+++ www/player/script.js
@@ -1,15 +1,5 @@
 const socket = io();
-const button = document.querySelector("button");
-const input = document.querySelector("input");
 const msg = document.querySelector("#msg");
-
-button.addEventListener("click", function(e) {
-  const text = input.value;
-  //enviarselo al servidor
-  socket.emit("message_evt", {msg: text});
-});
-
-fetch("player/");
 
 socket.on("message_evt", function(message){
   msg.innerHTML = message.msg;
