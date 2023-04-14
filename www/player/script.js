b489e689bfc1654d424a7f0545cfc6676be7f87f
Maria Garcia <100429121@alumnos.uc3m.es>
2023-04-14 11:54:18 +0200

    jfdksal

--- www/player/script.js
+++ www/player/script.js
@@ -8,6 +8,7 @@ socket.on("message_evt", function(message){
 
 socket.on("change_html", function(path){
   alert("Request to change window received")
+  alert(path)
   window.location.href = path;
 });
 
