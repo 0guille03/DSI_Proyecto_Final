85711adb3169a8de43a118576b1826285ec4cd4a
Maria Garcia <100429121@alumnos.uc3m.es>
2023-04-14 11:49:08 +0200

    debug

--- www/player/script.js
+++ www/player/script.js
@@ -4,8 +4,10 @@ const msg = document.querySelector("#msg");
 socket.on("message_evt", function(message){
   msg.innerHTML = message.msg;
 });
-/*
+
+
 socket.on("change_html", function(path){
+  alert("Request to change window received")
   window.location.href = path;
-});*/
+});
 
