0154ffc49a5d1a5ee12cc92332e24323f2333804
Maria Garcia <100429121@alumnos.uc3m.es>
2023-04-14 11:30:39 +0200

    vfds

--- www/player/script.js
+++ www/player/script.js
@@ -4,8 +4,8 @@ const msg = document.querySelector("#msg");
 socket.on("message_evt", function(message){
   msg.innerHTML = message.msg;
 });
-
+/*
 socket.on("change_html", function(path){
   window.location.href = path;
-});
+});*/
 
