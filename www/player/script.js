33c22902584d4f8a7c7bd2b06a4c7c6b98401227
Maria Garcia <100429121@alumnos.uc3m.es>
2023-04-14 12:04:06 +0200

    hg

--- www/player/script.js
+++ www/player/script.js
@@ -9,7 +9,7 @@ socket.on("change_html", function(path){
   alert("Request to change window received");
   alert(path.msg);
   window.location.href = path.msg;
-  var path_p = JSON.parse(path);
-  alert(path_p);
+  //var path_p = JSON.parse(path);
+  //alert(path_p);
 });
 
