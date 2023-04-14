e92592963dc69b82730a128d4b1a498761761ae0
Maria Garcia <100429121@alumnos.uc3m.es>
2023-04-14 12:00:05 +0200

    debugggg

--- www/player/script.js
+++ www/player/script.js
@@ -1,14 +1,15 @@
 const socket = io();
-const msg = document.querySelector("#msg");
 
-socket.on("message_evt", function(message){
+/*socket.on("message_evt", function(message){
   msg.innerHTML = message.msg;
-});
+});*/
 
 
 socket.on("change_html", function(path){
-  alert("Request to change window received")
-  alert(path.msg)
+  var path_p = JSON.parse(path);
+  alert("Request to change window received");
+  alert(path_p);
+  alert(path.msg);
   window.location.href = path.msg;
 });
 
