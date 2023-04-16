f69801543e2a18e5fabe88a353a1025c5ee59455
100428973 <100428973@alumnos.uc3m.es>
2023-04-17 00:56:56 +0200

    fixed

--- www/player/video_player.js
+++ www/player/video_player.js
@@ -1,8 +1,8 @@
 const socket = io();
 
 socket.on("set_video_to_play", function(video){
-    let video=document.querySelector("#playingVideo").src="../video/"+video.msg+".mp4";
-    video.play();
+    let video_html=document.querySelector("#playingVideo").src="../video/"+video.msg+".mp4";
+    video_html.play();
 });
 
 function getVideo(){
