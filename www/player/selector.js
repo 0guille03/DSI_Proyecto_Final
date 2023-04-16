b0bd5b8d50590ba4bd47fbb3cbf477997b5c522f
laumardi <100428953@alumnos.uc3m.es>
2023-04-16 11:40:58 +0200

    button changes color

--- www/player/selector.js
+++ www/player/selector.js
@@ -11,3 +11,21 @@ socket.on("change_html", function(path){
   
 });
 
+function changeButton(number){
+  if (number == '1'){
+    document.getElementById("button1").style.backgroundColor = "#6f8e91";
+    document.getElementById("button2").style.backgroundColor = "#D6E4E5";
+    document.getElementById("button3").style.backgroundColor = "#D6E4E5";
+  }
+  if (number == '2'){
+    document.getElementById("button1").style.backgroundColor = "#D6E4E5";
+    document.getElementById("button2").style.backgroundColor = "#6f8e91";
+    document.getElementById("button3").style.backgroundColor = "#D6E4E5";
+  }
+  if (number == '3'){
+    document.getElementById("button1").style.backgroundColor = "#D6E4E5";
+    document.getElementById("button2").style.backgroundColor = "#D6E4E5";
+    document.getElementById("button3").style.backgroundColor = "#6f8e91";
+  }
+}
+
