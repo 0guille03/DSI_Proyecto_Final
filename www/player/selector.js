11aff11137f9d06bfc525079b204562e8cb4f833
laumardi <100428953@alumnos.uc3m.es>
2023-04-16 11:50:42 +0200

    pantalla de resultados de búsqueda y gestos a mediaas

--- www/player/selector.js
+++ www/player/selector.js
@@ -1,14 +1,28 @@
 const socket = io();
 
-/*socket.on("message_evt", function(message){
-  msg.innerHTML = message.msg;
-});*/
 
-
-socket.on("change_html", function(path){
-  alert(path.msg);
+socket.on("change_html", function(path) {
   window.location.href = path.msg;
-  
+});
+
+socket.on("volume_up", function(){
+  document.getElementById("demo").innerHTML = "Último gesto detectado: subir volumen";
+});
+
+socket.on("volume_down", function(){
+  document.getElementById("demo").innerHTML = "Último gesto detectado: bajar volumen";
+});
+
+socket.on("go_back", function(){
+  document.getElementById("demo").innerHTML = "Último gesto detectado: ir hacia atrás";
+});
+
+socket.on("go_forward", function(){
+  document.getElementById("demo").innerHTML = "Último gesto detectado: ir hacia delante";
+});
+
+socket.on("toggle_overlay", function(){
+  document.getElementById("demo").innerHTML = "Último gesto detectado: quitar/poner overlay";
 });
 
 function changeButton(number){
