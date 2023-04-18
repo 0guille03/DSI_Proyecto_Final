ba4ec80c614d1e42f23becaa874eb80e7d416e4e
Replit user <>
2023-04-18 10:04:33 +0000

    Gestos a medias

--- www/controller/video_player.js
+++ www/controller/video_player.js
@@ -25,37 +25,38 @@ const constraints = {
   video: true,
 };
 
-function startCamera(){
+function startCamera() {
   navigator.mediaDevices
-  .getUserMedia(constraints)
-  .then((stream) => {
-    const videoTracks = stream.getVideoTracks();
-    console.log("Got stream with constraints:", constraints);
-    console.log(`Using video device: ${videoTracks[0].label}`);
-    stream.onremovetrack = () => {
-      console.log("Stream ended");
-    };
-    video.srcObject = stream;
-  })
-  .catch((error) => {
-    if (error.name === "ConstraintNotSatisfiedError") {
-      console.error(
-        `The resolution ${constraints.video.width.exact}x${constraints.video.height.exact} px is not supported by your device.`
-      );
-    } else if (error.name === "PermissionDeniedError") {
-      console.error(
-        "You need to grant this page permission to access your camera and microphone."
-      );
-    } else {
-      console.error(`getUserMedia error: ${error.name}`, error);
-    }
-  });}
+    .getUserMedia(constraints)
+    .then((stream) => {
+      const videoTracks = stream.getVideoTracks();
+      console.log("Got stream with constraints:", constraints);
+      console.log(`Using video device: ${videoTracks[0].label}`);
+      stream.onremovetrack = () => {
+        console.log("Stream ended");
+      };
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      if (error.name === "ConstraintNotSatisfiedError") {
+        console.error(
+          `The resolution ${constraints.video.width.exact}x${constraints.video.height.exact} px is not supported by your device.`
+        );
+      } else if (error.name === "PermissionDeniedError") {
+        console.error(
+          "You need to grant this page permission to access your camera and microphone."
+        );
+      } else {
+        console.error(`getUserMedia error: ${error.name}`, error);
+      }
+    });
+}
 
 function stopCamera() {
   const stream = video.srcObject;
   const tracks = stream.getTracks();
 
-  tracks.forEach(function (track) {
+  tracks.forEach(function(track) {
     track.stop();
   });
 
@@ -69,3 +70,72 @@ dropdown.addEventListener("click", function() {
   console.log("entre");
   dropdownMenu.classList.toggle("show");
 });
+
+//Code for gestures
+
+var last_tap = 0;
+
+var startX, startY, touchStartTimestamp;
+
+function handleTouchStart(event) {
+  console.log("handler2");
+  startX = event.touches[0].clientX;
+  startY = event.touches[0].clientY;
+  touchStartTimestamp = event.timeStamp;
+}
+
+function handleTouchEnd(event) {
+  var currX = event.changedTouches[0].clientX;
+  var currY = event.changedTouches[0].clientY;
+  var touchEndTimestamp = event.timeStamp;
+  if (Math.abs(currX - startX) < 50 && Math.abs(currY - startY) < 50 && Math.abs(touchEndTimestamp - touchStartTimestamp) > 500) {
+    toggleOverlay();
+  }
+}
+
+// Tilt left
+window.addEventListener("deviceorientation", function(event) {
+  var roll = event.gamma;
+  if (roll < -45) {
+    alert("go back")
+  }
+});
+
+// Tilt right
+window.addEventListener("deviceorientation", function(event) {
+  var roll = event.gamma;
+  if (roll > 45) {
+    alert("go forward")
+  }
+});
+
+// Tilt forward
+window.addEventListener("deviceorientation", function(event) {
+  var pitch = event.beta;
+  if (pitch < -45) {
+    alert("volume up");
+  }
+});
+
+// Tilt back
+window.addEventListener("deviceorientation", function(event) {
+  var pitch = event.beta;
+  if (pitch > 90) {
+    alert("volume down");
+  }
+});
+
+
+var container = document.getElementById("controls_container");
+
+function toggleOverlay() {
+
+  if (container.style.display === "none") {
+    container.style.display = "flex";
+  } else {
+    container.style.display = "none";
+  }
+}
+
+document.addEventListener("touchstart", handleTouchStart);
+document.addEventListener("touchend", handleTouchEnd);
